2a860eb9afdb3606a2d2cd8ff14bd790c42c5f98
Igor <k007.igor@gmail.com>
2023-03-21 11:56:28 +0500

    sections out-work end office + with mobiles, gallery-footer card editing

--- src/js/components/out-work.js
+++ src/js/components/out-work.js
@@ -1,27 +1,63 @@
 import Swiper, { Scrollbar } from 'swiper';
 Swiper.use([Scrollbar]);
 
+const tabswipers = document.querySelectorAll('.out-work__swiper');
+tabswipers.forEach((el) => {
+  let tabswiper = new Swiper(el, {
+    slidesPerView: 1.3,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+    },
 
-const tabswiper = new Swiper(".out-work__swiper", {
+    scrollbar: {
+      el: '.out-work__scrollbar',
+      draggable: true,
+      dragClass: 'swiper-scrollbar-drag',
+      dragSize: 200,
+    },
 
-  slidesPerView: 3.5,
-  spaceBetween: 16,
-  loop: true,
-  autoplay: {
-    delay: 1000,
-  },
-
-  autoplay: {
-    delay: 2500,
-    // disableOnInteraction: true,
-  },
-
-  scrollbar: {
-    el: '.out-work__scrollbar',
-    draggable: true,
-    // dragClass: '.swiper-scrollbar-drag',
-    // hide: true
-  }
+    breakpoints: {
+      440: {
+        slidesPerView: 2,
+        speceBetween: 10,
+      },
+      680: {
+        slidesPerView: 2.5,
+        speceBetween: 10,
+      },
+      849: {
+        slidesPerView: 2,
+        speceBetween: 10,
+      },
+      1020: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+      1220: {
+        slidesPerView: 3.5,
+        spaceBetween: 16,
+      },
+    },
+  });
 });
+
+
+const outWorkBtn = document.querySelector('.js-out-work__btn'),
+  itemNine = document.querySelectorAll('.out-work-item-none');
+
+outWorkBtn.addEventListener('click', () => {
+  itemNine.forEach((el) => {
+    el.classList.toggle('out-work-item-none')
+  })
+})
+
+
+
+
+
+
 
 
