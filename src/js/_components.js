9a18a6319083225d6e39ce35e93f0d94ac1cf39c
Igor <k007.igor@gmail.com>
2023-04-04 16:03:44 +0500

    section recommendations and mobile

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import './components/technologies';
 import './components/tabs';
 import './components/out-work';
 import './components/services';
+import './components/recommendations';
---
+++ src/js/components/recommendations.js
@@ -0,0 +1,37 @@
+import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation]);
+
+const recommendationsSwiper = new Swiper(".recommendations__swiper", {
+
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320:{
+      slidesPerView: 1,
+      speceBetween: 10,
+    },
+    645: {
+      slidesPerView: 1.7,
+      speceBetween: 30,
+    },
+    795: {
+      slidesPerView: 1,
+      speceBetween: 30,
+    },
+    1220: {
+      slidesPerView: 1.7,
+      speceBetween: 30,
+    },
+  }
+});
